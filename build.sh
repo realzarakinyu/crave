2f76fa0d1503bb6197af5da4ddd634e40d45f5f7
realzarakinyu <onlyzarakinyu@gmail.com>
2025-12-02 21:59:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 # BUILDING
 
+# Repo init
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 /opt/crave/resync.sh
+
+# remove old dir
 rm -rf vendor/xiaomi/sm4635-common
 rm -rf device/xiaomi/sm4635-common
 rm -rf vendor/xiaomi/warm
@@ -9,6 +12,8 @@ rm -rf device/xiaomi/warm
 rm -rf device/xiaomi/warm-kernel
 rm -rf hardware/xiaomi
 rm -rf hardware/qcom-caf/common
+
+# TREES
 git clone https://github.com/realzarakinyu/android_device_xiaomi_warm device/xiaomi/warm
 git clone https://github.com/realzarakinyu/android_vendor_xiaomi_warm vendor/xiaomi/warm
 git clone https://github.com/realzarakinyu/android_vendor_xiaomi_sm4635-common vendor/xiaomi/sm4635-common
@@ -16,6 +21,8 @@ git clone https://github.com/realzarakinyu/android_device_xiaomi_sm4635-common d
 git clone https://github.com/realzarakinyu/android_device_xiaomi_warm-kernel device/xiaomi/warm-kernel
 git clone https://github.com/realzarakinyu/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+
+# build
 source build/envsetup.sh
 lunch lineage_warm-bp2a-userdebug
 mka bacon

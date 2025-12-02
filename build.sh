87af506a615068290ac00d1d733b3ea3efde3a7e
realzarakinyu <onlyzarakinyu@gmail.com>
2025-12-02 22:56:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Repo init
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 /opt/crave/resync.sh
+echo "======= Repo Syncing, This going to take forever, so just go touch grass ======"
 
 # remove old dir
 rm -rf vendor/xiaomi/sm4635-common
@@ -13,8 +14,9 @@ rm -rf device/xiaomi/warm-kernel
 rm -rf hardware/xiaomi
 rm -rf hardware/qcom-caf/common
 rm -rf build/soong/fsgen
+echo "======= Removed Old Directories ======"
 
-# TREES
+# Trees cloning
 git clone https://github.com/xiaomi-warm-devs/android_device_xiaomi_warm -b lineage-23.0 device/xiaomi/warm
 git clone https://github.com/xiaomi-warm-devs/android_vendor_xiaomi_warm -b lineage-23.0 vendor/xiaomi/warm
 git clone https://github.com/xiaomi-warm-devs/android_device_xiaomi_sm4635-common -b lineage-23.0 device/xiaomi/sm4635-common
@@ -22,8 +24,18 @@ git clone https://github.com/xiaomi-warm-devs/android_vendor_xiaomi_sm4635-commo
 git clone https://github.com/xiaomi-warm-devs/android_device_xiaomi_warm-kernel -b lineage-23.0 device/xiaomi/warm-kernel
 git clone https://github.com/xiaomi-warm-devs/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+echo "====================================================================================================================="
+echo "=========================================== Trees Cloned Succesfully ================================================"
+echo "====================================================================================================================="
+
+# Export
+export BUILD_USERNAME=ZaraKinYu
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
 
 # build
 source build/envsetup.sh
 lunch lineage_warm-bp2a-userdebug
 mka bacon
+echo "======= Build Done Successfully ======"
